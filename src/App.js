d5f377783ddcfc0436a376af60530ba720be4189
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-12-30 00:00:10 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="font-outfit bg-dark-blue md:pt-6 lg:pt-8">
+      kl
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        red: '#FC4747',
+        'dark-blue': '#10141E',
+        'semi-dark-blue': '#161D2F',
+        'grayish-blue': '#5A698F',
+      },
+      fontFamily: {
+        'outfit': ['Outfit', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
