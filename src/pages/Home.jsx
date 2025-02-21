6a2c4e0507a6f31acffa26f9630e623eb3b2f59d
aleksi-svaridze <aleksi.svaridze08b@geolab.edu.ge>
2025-02-21 18:02:19 +0400

    Add animation property to caroucel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,22 +24,20 @@ function Home() {
   };
 
   const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      items: 2,
+      partialVisibilityGutter: 255, // this is needed to tell the amount of px that should be visible.
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      breakpoint: { max: 1024, min: 375 },
+      items: 1,
+      partialVisibilityGutter: 235, // this is needed to tell the amount of px that should be visible.
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 375, min: 0 },
       items: 1,
+      partialVisibilityGutter: 235, // this is needed to tell the amount of px that should be visible.
     },
   };
 
@@ -50,65 +48,22 @@ function Home() {
 
         {/* <div className="scroll-bar rounded-l-lg flex items-center gap-x-4 md:gap-x-10 mb-10 overflow-y-hidden"> */}
         <Carousel
-          className="bg-red"
+          sliderClass={"gap-x-4 md:gap-x-10"}
           swipeable={true}
           draggable={true}
           showDots={false}
           responsive={responsive}
-          infinite={true}
+          partialVisible={true}
+          removeArrowOnDeviceType={[
+            "superLargeDesktop",
+            "desktop",
+            "tablet",
+            "mobile",
+          ]}
           autoPlay={true}
-          autoPlaySpeed={3000}
-          keyBoardControl={true}
-          customTransition="all .5"
-          transitionDuration={500}
-          containerClass="carousel-container"
-          removeArrowOnDeviceType={["desktop", "tablet", "mobile"]}
-          dotListClass="custom-dot-list-style"
-          itemClass="carousel-item-padding-40-px"
-          focusOnSelect={true}
+          customTransition="all 2s ease-in-out"
+          autoPlaySpeed={4000}
         >
-          {/* <div>
-            <img
-              src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=1459&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://images.unsplash.com/photo-1478720568477-152d9b164e26?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://images.unsplash.com/photo-1543536448-d209d2d13a1c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://images.unsplash.com/photo-1595769816263-9b910be24d5f?q=80&w=1479&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://images.unsplash.com/photo-1524712245354-2c4e5e7121c0?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://images.unsplash.com/photo-1635100431192-180372735b06?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-          </div> */}
           {trending
             ? trending.map((movie) => (
                 <TrendingCard
@@ -123,19 +78,6 @@ function Home() {
               ))
             : "No Movies"}
         </Carousel>
-        {/* {trending
-            ? trending.map((movie) => (
-                <TrendingCard
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.release_date}
-                  category={movie.media_type}
-                  genre={movie.genre}
-                  movieTitle={movie.title}
-                  src={movie.backdrop_path}
-                />
-              ))
-            : "No Movies"} */}
         {/* </div> */}
       </div>
 
